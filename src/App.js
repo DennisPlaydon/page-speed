c003613fb724036bebd63aa5b705daabf1b4a146
Dennis Playdon <dennis9953@gmail.com>
2019-02-03 17:09:36 +1300

    Add title component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import PageInput from "./components/PageInput";
+import Title from "./components/Title";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PageInput />
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <Title />
+                <PageInput />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class PageInput extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title-container__title"> Page Speed</h1>
+        <h3 className="title-container__subtitle">
+          Find out how fast any website is
+        </h3>
+      </div>
+    );
+  }
+}
+export default PageInput;
